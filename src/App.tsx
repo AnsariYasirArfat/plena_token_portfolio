6cc539fb4f41c6e4082bb674e7adbd43cfd66478
AnsariYasirArfat <ansariyaseer786@gmail.com>
2025-08-29 23:34:17 +0530

    Initialize React project with TypeScript, Vite, and Tailwind CSS. Add basic project structure including components, store, and utility functions.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen text-plena-lime text-3xl font-extrabold bg-plena-base">
+      <h1 className="bg-plena-component p-8 rounded-3xl">Plena finance</h1>
+    </div>
+  );
+}
+
+export default App;
